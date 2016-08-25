4f6d696da242df705f23ea99125f44b735e9c44c
mswatermelon <aristarkhova_veronika@mail.ru>
2016-08-25 22:16:23 +0300

    Init

---
+++ entry.js
@@ -0,0 +1,31 @@
+Handlebars.registerHelper('formatTime', function(time) {
+    var minutes = parseInt(time / 60),
+        seconds = time - minutes * 60;
+
+    minutes = minutes.toString().length === 1 ? '0' + minutes : minutes;
+    seconds = seconds.toString().length === 1 ? '0' + seconds : seconds;
+
+    return minutes + ':' + seconds;
+});
+
+Handlebars.registerHelper('formatDate', function(ts) {
+    return new Date(ts * 1000).toLocaleString();
+});
+
+Handlebars.registerHelper('clear', function(val) {
+    return val || 0;
+});
+
+new Promise(function(resolve) {
+    window.onload = resolve;
+}).then(function() {
+    return Model.login(5583715, 2 | 4 | 8 | 8192);
+}).then(function() {
+    return Model.getUser().then(function(users) {
+        header.innerHTML = View.render('header', users[0]);
+    });
+}).catch(function(e) {
+    console.error(e);
+    alert('Ошибка: ' + e.message);
+});
+
